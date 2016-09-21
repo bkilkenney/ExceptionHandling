7d46748cd255d4e19ed7e87a892befaf8236f26a
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-21 14:03:32 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Try-Catch construct
+
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                string numberText = Console.ReadLine();
+                int number = int.Parse(numberText);
+                Console.WriteLine("Your number is " + number);
+            }
+
+            catch (FormatException objectName)
+            {
+                Console.WriteLine("That is not a valid number");
+            }
+        }
+    }
+}
