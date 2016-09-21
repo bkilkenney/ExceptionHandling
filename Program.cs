ed9766639b664bc9def0f8544a18b5779ba52d7f
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-21 14:38:52 -0400

    Last half hour

--- Program.cs
+++ Program.cs
@@ -12,18 +12,54 @@ namespace ExceptionHandling
         {
             //Try-Catch construct
 
+            //try
+            //{
+            //    Console.WriteLine("Enter a number: ");
+            //    string numberText = Console.ReadLine();
+            //    int number = int.Parse(numberText);
+            //    Console.WriteLine("Your number is " + number);
+            //}
+            //catch (FormatException objectName)
+            //{
+            //    Console.WriteLine("That is not a valid number");
+            //}
+
+            //Try - Throw - Need an example, none in slides
+
+            ////Try - Finally
+            //try
+            //{
+            //    Console.WriteLine("Enter a number: ");
+            //    string numberText = Console.ReadLine();
+            //    int number = int.Parse(numberText);
+            //    Console.WriteLine("Your number is " + number);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Thanks for playing!");
+            //}
+
+            //declare 2 variables
+            float result;
+            int num;
+
+            //Prompt the user for a number
+            Console.Write("Enter Number: ");
+
+            //Try block will run the code in the block
             try
             {
-                Console.WriteLine("Enter a number: ");
-                string numberText = Console.ReadLine();
-                int number = int.Parse(numberText);
-                Console.WriteLine("Your number is " + number);
+                num = int.Parse(Console.ReadLine());
+
+                result = (float)Math.Sqrt(num);   //This is, which is similar to Parse or Convert
+                Console.WriteLine("The result is: {0}", result);
             }
 
-            catch (FormatException objectName)
+
+            //Catch block will handle the exception
+            catch (Exception)
             {
-                Console.WriteLine("That is not a valid number");
+            }
             }
         }
     }
-}
